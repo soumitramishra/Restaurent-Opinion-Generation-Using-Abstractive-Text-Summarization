d99bf7e30688d76e68c61752a994be65365b0389
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2019-04-08 23:29:57 -0400

    add eos tags

--- write_to_individual_files.py
+++ write_to_individual_files.py
@@ -7,6 +7,7 @@ Created on Fri Mar 15 16:48:50 2019
 import os
 import pandas as pd
 from concurrent.futures.thread import ThreadPoolExecutor
+from nltk import sent_tokenize
 
 path = os.getcwd()
 
@@ -16,12 +17,15 @@ bis_id = list(set(combine_data['business_id']))
 def write_to_file(i):
     dt = combine_data.loc[combine_data['business_id'] == i]
     dt.drop(dt.columns[0], axis = 1, inplace= True)
-    text = dt['text'].astype(str)
+    text = dt['text'].values
+    for t in text:
+        sents = sent_tokenize(t)
+        sents = [s+' <eos> ' for s in sents]
+    text = ''.join(sents)
     res_name = ''.join(set(dt['name']))+'_'+i
     
     with open (path+'\\reviews\\'+res_name+'.txt','w',encoding = 'utf-8') as f:
-        for tex in text:
-            f.write(tex)
+         f.write(text)
     
     return('')
 
