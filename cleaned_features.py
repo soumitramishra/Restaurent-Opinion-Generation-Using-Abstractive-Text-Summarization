401b8ed0b45f5828b96274f863aaabf762e6224c
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2019-04-08 23:33:40 -0400

    cleanup and generalize

--- cleaned_features.py
+++ cleaned_features.py
@@ -1,16 +1,16 @@
+import os
 import pandas as pd
 import string
 import gensim
 import collections
 from nltk.tokenize import TweetTokenizer
 
-
-word2vec = gensim.models.KeyedVectors.load_word2vec_format(
-        'D:/CCIS/NLP_LuWang/Assignment/Assignment2/Question4/GoogleNews-vectors-negative300.bin', binary=True)
+path = os.getcwd()
+word2vec = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 index2word_set = word2vec.vocab
 
 
-path = "C:/Users/Rajath Kashyap/Downloads/Yardbird Southern Table & Bar_faPVqws-x-5k2CQKDNtHxw.txt"
+#path = "C:/Users/Rajath Kashyap/Downloads/Yardbird Southern Table & Bar_faPVqws-x-5k2CQKDNtHxw.txt"
 
 def word2vec_matrix(word_list):
     words = []
@@ -21,8 +21,8 @@ def word2vec_matrix(word_list):
     return pd.DataFrame(words)
 
 
-def read_reviews():
-     with open (path, "r", encoding="utf-8") as f:
+def read_reviews(file):
+     with open (file, "r", encoding="utf-8") as f:
          return [x.strip() for x in f.readlines()]
     
     
@@ -38,10 +38,9 @@ def pre_process(data):
     return words, word_counter            
     
 
+files = os.listdir(path = path+'\\reviews')
+files = [path+'\\'+f for i in files]
 
-data = read_reviews()
+data = read_reviews(files[0])
 data, word_count = pre_process(data)
 df = word2vec_matrix(word_count)
-
-
-
