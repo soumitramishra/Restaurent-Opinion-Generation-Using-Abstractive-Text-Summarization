8bdb63eec7950d7d3f8c0881cc19422cd38184f3
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2019-04-12 16:28:14 -0400

    recursion

--- kavitha-summaries.py
+++ kavitha-summaries.py
@@ -54,8 +54,8 @@ def get_ngram_containing(unigram,bigram):
             new_dict[k] = v
     return new_dict
 
-def ngram_tuple(ngram):
-    return [(k.split(' ')[0],k.split(' ')[1]) for k,v in ngram.items()]
+#def ngram_tuple(ngram):
+#    return [(k.split(' ')[0],k.split(' ')[1]) for k,v in ngram.items()]
 
 
 def generate_scored_ngrams(seed):
@@ -63,15 +63,15 @@ def generate_scored_ngrams(seed):
     '''
     new_dict = {}
     for s in tqdm(seed):
-        for s2 in seed:
-            if s != s2:
-                if s[1] == s2[0]:
-                    val = tuple(set(s+s2))
-                    tx = ''
-                    for i in val:
-                        tx = tx+' '+i
-                      #  print(tx)
-                    new_dict[val] = readability.getmeasures(tx.strip(), lang='en')['readability grades']['FleschReadingEase']
+        for S in seed:
+            if s != S:
+                s1 = s.split(' ')
+                s2 = S.split(' ')
+                if s1[-1] == s2[0]:
+                    if not any(e in s2[1:] for e in s1[:-1]):
+                        s1.extend(s2[1:])
+                        val = ' '.join(s1)
+                        new_dict[val.strip()] = readability.getmeasures(val.strip(), lang='en')['readability grades']['FleschReadingEase']
     
     return new_dict
 
@@ -97,6 +97,7 @@ def representativeness(ngram):
     '''
     Calculate representativeness
     '''
+    ngram = ngram.split(' ')
     window = floor(len(ngram)/2)
     s_rep = []
     for n,gram in enumerate(ngram):
@@ -108,8 +109,8 @@ def representativeness(ngram):
     
     return mean(s_rep)
 
-#data = get_data('Four Peaks Brewing_JzOp695tclcNCNMuBl7oxA.txt')
-data = get_data("Madison Marriott West_yFKNTMyCQZ92M91A_yavhw.txt")
+data = get_data('Four Peaks Brewing_JzOp695tclcNCNMuBl7oxA.txt')
+#ata = get_data("Rajkapuri's Paan & Snacks_gFUSJwVzEDERbaU9FVKfRA.txt")
 unig = generate_ngrams(data, 1)
 bigrams = generate_ngrams(data,2)
 
@@ -121,14 +122,14 @@ for bi,v in bigrams.items():
         seed_dict[bi] = v
 
 d = get_ngram_containing(unig,seed_dict)
-bigram_tup = ngram_tuple(d)
-scores = generate_scored_ngrams(bigram_tup)
+#bigram_tup = ngram_tuple(d)
+#scores = generate_scored_ngrams(list(d.keys()))
 unigram_prob = prob(unig)
 bigram_prob = prob(seed_dict)
     
-with open('scores.txt','w') as f:
-    for k,v in scores.items():
-        print(str(k)+' '+str(v), file = f)
+#with open('scores.txt','w') as f:
+#    for k,v in scores.items():
+#        print(str(k)+' '+str(v), file = f)
 
 
 #rep_dict= {}
@@ -136,27 +137,80 @@ with open('scores.txt','w') as f:
 #    rep_dict[ngram] = representativeness(ngram)
 #    with open('rep.txt','a+') as f:
 #        print(str(ngram)+' '+str(rep_dict[ngram]), file = f)
+#srep = {}
+#for ngram in tqdm(scores.keys()):
+#    srep[ngram] = representativeness(ngram)
 
-futures_list = []
-from concurrent.futures.thread import ThreadPoolExecutor
-with ThreadPoolExecutor(max_workers=12) as executor:
-    for arg in scores.keys():
-        futures_list.append((executor.submit(representativeness, arg),arg)) 
+#futures_list = []
+#from concurrent.futures.thread import ThreadPoolExecutor
+#with ThreadPoolExecutor(max_workers=12) as executor:
+#    for arg in scores.keys():
+#        futures_list.append((executor.submit(representativeness, arg),arg)) 
 
-with open('rep.txt','w', newline = '', encoding = 'utf-8') as f:
-                     for futures in tqdm(futures_list):
-                         res = futures[0].result()
-                         print(str(futures[1])+' '+str(res), file = f)
-                         
-srep = {i[1]:i[0].result() for i in futures_list}                         
-srep_thresh = 0.7
-read_thresh =120
-srep_new = {k:v for k,v in srep.items() if v >= srep_thresh}
-score_new = {k:v for k,v in scores.items() if v>read_thresh}
+#with open('rep.txt','w', newline = '', encoding = 'utf-8') as f:
+#                     for futures in tqdm(futures_list):
+#                         res = futures[0].result()
+#                         print(str(futures[1])+' '+str(res), file = f)
+#                         
+#srep = {i[1]:i[0].result() for i in futures_list}                         
+#srep_thresh = 0.7
+#read_thresh =120
+#srep_new = {k:v for k,v in srep.items() if v >= srep_thresh}
+#score_new = {k:v for k,v in scores.items() if v>read_thresh}
 # k in list(srep_new.keys()) k in list(srep_new.keys())
-t = sorted(scores.items(), key=lambda x:-x[1])[:20]
-t1 = sorted(srep.items(), key=lambda x:-x[1])[:20]
+#t = sorted(scores.items(), key=lambda x:-x[1])[:20]
+#t1 = sorted(srep.items(), key=lambda x:-x[1])[:20]
 #def generate_candidates(seed, candidate, srd_th, srp_th):
 #    if
 #    
 #    return
+
+
+candidate = {}
+    
+top500 = bigrams.most_common(1000)
+
+def forallseeds(bigrams):
+    top500 = bigrams.most_common(500)
+    clist = [i[0] for i in top500]
+    for bg in tqdm(clist):
+        generate_candidates(bg,0,-1,clist)
+    
+    return 
+
+def generate_candidates(bg, rd_th, rp_th, clist):
+    candidate[bg] = {'rd':0, 'rp':0}
+    candidate[bg]['rd'] = readability.getmeasures(bg, lang='en')['readability grades']['FleschReadingEase']
+    candidate[bg]['rp'] = representativeness(bg)
+    gen_list = [i for i in clist]
+    
+    if candidate[bg]['rd'] < rd_th or candidate[bg]['rp'] < rp_th:
+        return
+    else:
+        cl = []
+        for i in gen_list:
+            tmp = i.split(' ')
+            tmp.reverse()
+            val = ' '.join(bg.split(' ')[-2:])
+            if bg.split(' ')[-1] == tmp[1] and val != ' '.join(tmp):
+                cl.append(bg+' '+' '.join(i.split(' ')[1:]))
+    
+    for gram in cl:
+        generate_candidates(gram, 00,-1, clist)
+    
+
+
+
+forallseeds(bigrams)
+
+cd = {}
+for k,v in candidate.items():
+    a = []
+    for g,h in v.items():
+        a.append(h)
+    
+    cd[k] = sum(a)
+
+
+
+
