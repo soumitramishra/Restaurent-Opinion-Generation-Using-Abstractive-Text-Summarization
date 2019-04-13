60a635700e356757660170a3206cdd51be793088
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2019-04-13 14:58:15 -0400

    make it importable

--- load_data.py
+++ load_data.py
@@ -7,14 +7,7 @@ Created on Sat Apr 13 11:26:38 2019
 
 import os
 import re
-
-path = os.getcwd()
-reviews = path+'\\summaries handwritten\\'
-
-datasets={'reviews':reviews}
-data_categories=["training","validation","test"]
-data={'review':[],'summaries':[]}
-
+from tqdm import tqdm
 
 def load_data(dire,category):
     """dataname refers to either training, test or validation"""
@@ -24,8 +17,7 @@ def load_data(dire,category):
 
 def parsetext(dire,category,filename):
     with open("%s\\%s"%(dire+category,filename),'r',encoding="Latin-1") as readin:
-        print("file read successfully")
-        text=readin.read()
+       text=readin.read()
     return text.lower().replace('<eos>','')
 
 
@@ -36,12 +28,13 @@ def cleantext(text):
     return text.split('summary:')
 
 
+def make_data_dict(filenames, x,y):
+    data={'review':[],'summaries':[]}
+    for f in tqdm(filenames):
+        dat = cleantext(parsetext(x['reviews'],y[0], '%s' %f))
+        data['review'].append(dat[0])
+        data['summaries'].append(dat[1])
+    print('All files read successfully...')
+    return data 
 
-filenames=load_data(datasets['reviews'],data_categories[0])
-
-for f in filenames:
-    dat = cleantext(parsetext(datasets['reviews'], data_categories[0], '%s' %f))
-    print(str(len(dat))+'----'+f)
-    data['review'].append(dat[0])
-    data['summaries'].append(dat[1])
     
