b7a6b57d8f9be13d0fcd1ec9fd2b90ceb7bd5cc2
RajathKashyap <44442941+RajathKashyap@users.noreply.github.com>
2019-04-15 20:33:06 -0400

    Update kavitha-summaries.py

--- kavitha-summaries.py
+++ kavitha-summaries.py
@@ -6,6 +6,7 @@ from statistics import median, mean
 import readability
 import string
 from tqdm import tqdm
+import itertools
 
 def get_data(filename):
     '''Get data from file'''
@@ -109,63 +110,31 @@ def representativeness(ngram):
     
     return mean(s_rep)
 
-data = get_data('Four Peaks Brewing_JzOp695tclcNCNMuBl7oxA.txt')
+
+def generate_seed_bigrams(unigrams):
+    b = itertools.combinations(unigrams.keys(), 2)
+    bigrams = []
+    for o in b:
+        bigrams.append(o)
+    return bigrams
+
+data = get_data('D:\\CCIS\\NLP_LuWang\\NLP project\\reviews\\Four Peaks Brewing.txt')
 #ata = get_data("Rajkapuri's Paan & Snacks_gFUSJwVzEDERbaU9FVKfRA.txt")
 unig = generate_ngrams(data, 1)
-bigrams = generate_ngrams(data,2)
+uni_gram = Counter(unig).most_common(30)
+x = {i[0]:i[1] for i in uni_gram}
+bigrams = generate_seed_bigrams(x)
+bigrams = [' '.join(tup) for tup in bigrams]
+
+bigrams = Counter(bigrams)
 
 uni = list(unig.keys())
-seed_dict = {}
-for bi,v in bigrams.items():
-    b = bi.split(' ')
-    if b[0] in uni and b[1] in uni:
-        seed_dict[bi] = v
-
 d = get_ngram_containing(unig,seed_dict)
 #bigram_tup = ngram_tuple(d)
 #scores = generate_scored_ngrams(list(d.keys()))
 unigram_prob = prob(unig)
 bigram_prob = prob(seed_dict)
     
-#with open('scores.txt','w') as f:
-#    for k,v in scores.items():
-#        print(str(k)+' '+str(v), file = f)
-
-
-#rep_dict= {}
-#for ngram in tqdm(scores.keys()):
-#    rep_dict[ngram] = representativeness(ngram)
-#    with open('rep.txt','a+') as f:
-#        print(str(ngram)+' '+str(rep_dict[ngram]), file = f)
-#srep = {}
-#for ngram in tqdm(scores.keys()):
-#    srep[ngram] = representativeness(ngram)
-
-#futures_list = []
-#from concurrent.futures.thread import ThreadPoolExecutor
-#with ThreadPoolExecutor(max_workers=12) as executor:
-#    for arg in scores.keys():
-#        futures_list.append((executor.submit(representativeness, arg),arg)) 
-
-#with open('rep.txt','w', newline = '', encoding = 'utf-8') as f:
-#                     for futures in tqdm(futures_list):
-#                         res = futures[0].result()
-#                         print(str(futures[1])+' '+str(res), file = f)
-#                         
-#srep = {i[1]:i[0].result() for i in futures_list}                         
-#srep_thresh = 0.7
-#read_thresh =120
-#srep_new = {k:v for k,v in srep.items() if v >= srep_thresh}
-#score_new = {k:v for k,v in scores.items() if v>read_thresh}
-# k in list(srep_new.keys()) k in list(srep_new.keys())
-#t = sorted(scores.items(), key=lambda x:-x[1])[:20]
-#t1 = sorted(srep.items(), key=lambda x:-x[1])[:20]
-#def generate_candidates(seed, candidate, srd_th, srp_th):
-#    if
-#    
-#    return
-
-
 candidate = {}
     
 top500 = bigrams.most_common(1000)
